0b5889becafcd7efedaba65eb2e83055dd4749e8
Eric GELOEN <geloen.eric@gmail.com>
2012-07-22 21:31:12 +0200

    [DBAL] Add mysqli to the built-in drivers + Add all mysqli common tests (connection + schema manager)

---
+++ tests/Fridge/Tests/DBAL/Connection/MysqliConnectionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Fridge DBAL package.
+ *
+ * (c) Eric GELOEN <geloen.eric@gmail.com>
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fridge\Tests\DBAL\Connection;
+
+use Fridge\Tests\ConnectionUtility,
+    Fridge\Tests\Fixture\MySQLFixture;
+
+/**
+ * Mysqli connection test.
+ *
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class MysqliConnectionTest extends AbstractConnectionTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    static public function setUpBeforeClass()
+    {
+        if (ConnectionUtility::hasConnection(ConnectionUtility::MYSQLI)) {
+            self::$fixture = new MySQLFixture();
+        }
+
+        parent::setUpBeforeClass();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        if (ConnectionUtility::hasConnection(ConnectionUtility::MYSQLI)) {
+            $this->connection = ConnectionUtility::getConnection(ConnectionUtility::MYSQLI);
+        }
+
+        parent::setUp();
+    }
+}
